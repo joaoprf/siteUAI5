b770f7adf37113b028220bdf0de19df7c33c6c2c
João Pedro <40777175+joaoprf@users.noreply.github.com>
2025-05-20 20:00:21 -0300

    Retirando o ícon do X

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -159,20 +159,7 @@ const Contact = () => {
               <div className="mt-8">
                 <h3 className="font-bold text-gray-900 mb-4">Siga-nos</h3>
                 <div className="flex space-x-4">
-                  <a 
-                    href="#" 
-                    className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-600 hover:bg-green-800 hover:text-white transition-colors"
-                  >
-                    <svg 
-                      xmlns="http://www.w3.org/2000/svg" 
-                      width="20" 
-                      height="20" 
-                      viewBox="0 0 24 24" 
-                      fill="currentColor"
-                    >
-                      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
-                    </svg>
-                  </a>
+                  
                   <a 
                     href="#" 
                     className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-600 hover:bg-green-800 hover:text-white transition-colors"
