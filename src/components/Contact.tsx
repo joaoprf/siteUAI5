9a51bd4fc5b297df50c92e88e160cff36bd6448a
João Pedro <40777175+joaoprf@users.noreply.github.com>
2025-06-10 20:49:59 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -106,14 +106,14 @@ const Contact = () => {
                   </div>
                 </div>
                 
-                <div className="flex items-start space-x-4">
-    <a href="wa.me/5531997835768" className="flex items-start space-x-4 group">
-        <div className="flex-shrink-0 mt-1">
+               <div className="flex items-start space-x-4">
+    <a href="https://wa.me/5531997835768" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 group">
+        <div className="flex-shrink-0">
             <div className="w-10 h-10 rounded-full bg-green-100 flex items-center justify-center text-green-800 group-hover:bg-green-200 group-hover:text-green-900 transition-colors">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
+                    width="24"
+                    height="24"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
@@ -121,14 +121,11 @@ const Contact = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                 >
-                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                    <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
                 </svg>
             </div>
         </div>
-        <div>
-            <h3 className="font-bold text-gray-900 mb-1 group-hover:text-green-800 transition-colors">Whatsapp</h3>
-            
-        </div>
+        <h3 className="font-bold text-gray-900 group-hover:text-green-800 transition-colors">Whatsapp</h3>
     </a>
 </div>
                 
