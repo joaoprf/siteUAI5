351b7af6c7eaa8e45c1f108ca8aa8ad57787f7a4
João Pedro <40777175+joaoprf@users.noreply.github.com>
2025-06-10 20:29:45 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -107,28 +107,30 @@ const Contact = () => {
                 </div>
                 
                 <div className="flex items-start space-x-4">
-                  <div className="flex-shrink-0 mt-1">
-                    <div className="w-10 h-10 rounded-full bg-green-100 flex items-center justify-center text-green-800">
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        width="20" 
-                        height="20" 
-                        viewBox="0 0 24 24" 
-                        fill="none" 
-                        stroke="currentColor" 
-                        strokeWidth="2" 
-                        strokeLinecap="round" 
-                        strokeLinejoin="round"
-                      >
-                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                      </svg>
-                    </div>
-                  </div>
-                  <div>
-                    <h3 className="font-bold text-gray-900 mb-1">Telefone</h3>
-                    <p className="text-gray-700">(31)99783-5768</p>
-                  </div>
-                </div>
+    <a href="wa.me/5531997835768" className="flex items-start space-x-4 group">
+        <div className="flex-shrink-0 mt-1">
+            <div className="w-10 h-10 rounded-full bg-green-100 flex items-center justify-center text-green-800 group-hover:bg-green-200 group-hover:text-green-900 transition-colors">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+            </div>
+        </div>
+        <div>
+            <h3 className="font-bold text-gray-900 mb-1 group-hover:text-green-800 transition-colors">Whatsapp</h3>
+            
+        </div>
+    </a>
+</div>
                 
                 <div className="flex items-start space-x-4">
                   <div className="flex-shrink-0 mt-1">
