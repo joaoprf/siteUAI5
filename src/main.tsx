8beb84cd369bfb3bdac82ef457879f69cf03f944
João Pedro <40777175+joaoprf@users.noreply.github.com>
2026-06-09 17:25:47 -0300

    Wrap routes in I18nProvider for internationalization

--- src/main.tsx
+++ src/main.tsx
@@ -7,30 +7,33 @@ import BlogPost from "./components/BlogPost.tsx";
 import React from "react";
 import Layout from "./components/Layout.tsx";
 import ChatbotWhatsApp from "./components/ChatbotWhatsApp.tsx";
+import { I18nProvider } from "./i18n/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/chatbot-whatsapp" element={<ChatbotWhatsApp />} />
-        <Route
-          path="/blog"
-          element={
-            <Layout>
-              <Blog />
-            </Layout>
-          }
-        />
-        <Route
-          path="/blog/:slug"
-          element={
-            <Layout>
-              <BlogPost />
-            </Layout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <I18nProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/chatbot-whatsapp" element={<ChatbotWhatsApp />} />
+          <Route
+            path="/blog"
+            element={
+              <Layout>
+                <Blog />
+              </Layout>
+            }
+          />
+          <Route
+            path="/blog/:slug"
+            element={
+              <Layout>
+                <BlogPost />
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </I18nProvider>
   </React.StrictMode>
 );
